96b194e84233fbb7d853350dc4b8616a4a1442bb
Hiro Nguyen <78646685+imminist@users.noreply.github.com>
2023-03-19 19:44:06 +0900

    Applying Validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,48 @@
-import React, { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
+import { useForm, FieldValues } from 'react-hook-form'
+
+interface FormData {
+  name: string
+  age: number
+}
 
 const Form = () => {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault()
-    console.log('Submitted')
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+  const onSubmit = (data: FieldValues) => console.log(data)
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          {...register('name', { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === 'required' && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === 'minLength' && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          {...register('age')}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
